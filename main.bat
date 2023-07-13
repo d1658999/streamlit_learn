38b96b731a527350aae40b53abc4ab696f79e716
PriceWu <@google.com>
2023-07-13 14:53:47 +0800

    rename and try some new to learn

---
+++ main.bat
@@ -0,0 +1,4 @@
+@echo off
+
+start cmd /k .\Python311\python.exe -m streamlit run streamlit_ui.py
+
---
+++ streamlit_ui.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
+
+
+def main():
+    with st.form("my_form"):
+        st.write("Inside the form")
+        slider_val = st.slider("Form slider", -40, 30)
+        checkbox_val = st.checkbox("Form checkbox")
+
+        # Every form must have a submit button.
+        submitted = st.form_submit_button("Submit")
+        if submitted:
+            st.write("slider", slider_val, "checkbox", checkbox_val)
+
+    st.write("Outside the form")
+
+
+if __name__ == "__main__":
+    main()
